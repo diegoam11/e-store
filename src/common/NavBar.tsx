fb2254cc4ccd7a522274c321969b042d0e98791f
diegoam11 <diego.alvarez2@unmsm.com>
2024-03-17 19:31:56 -0500

    changing login text to lower case

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -45,6 +45,7 @@ export const NavBar: React.FC = () => {
                     </button>
                 </nav>
                 <button className="inline-flex items-center bg-indigo-600 border-0 py-1 px-4 focus:outline-none  text-white rounded text-base mt-4 md:mt-0">Inicia sesión
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
                 </button>
