2574dc1866c3f8e74bed06c8bf127fa95fe08767
diegoam11 <diego.alvarez2@unmsm.com>
2024-03-17 19:25:38 -0500

    added loading spinners

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -22,29 +22,29 @@ export const NavBar: React.FC = () => {
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a href='/' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <img src={diamondIcon} className="w-8 h-8" />
-                    <span className="ml-2 text-xl">Diamond</span>
+                    <span className="text-xl">Diamond</span>
                 </a>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
                     <button
-                        className={`mr-5 hover:text-gray-900 ${selectedButton === Category.SNACKS ? 'border-b-2 border-black' : ''}`}
+                        className={`mr-5 hover:text-gray-900 ${selectedButton === Category.SNACKS ? 'border-b-2 border-indigo-600' : ''}`}
                         onClick={() => handleButtonClick(Category.SNACKS)}
                     >
                         {Category.SNACKS}
                     </button>
                     <button
-                        className={`mr-5 hover:text-gray-900 ${selectedButton === Category.LICORES ? 'border-b-2 border-black' : ''}`}
+                        className={`mr-5 hover:text-gray-900 ${selectedButton === Category.LICORES ? 'border-b-2 border-indigo-600' : ''}`}
                         onClick={() => handleButtonClick(Category.LICORES)}
                     >
                         {Category.LICORES}
                     </button>
                     <button
-                        className={`mr-5 hover:text-gray-900 ${selectedButton === Category.TODO ? 'border-b-2 border-black' : ''}`}
+                        className={`mr-5 hover:text-gray-900 ${selectedButton === Category.TODO ? 'border-b-2 border-indigo-600' : ''}`}
                         onClick={() => handleButtonClick(Category.TODO)}
                     >
                         {Category.TODO}
                     </button>
                 </nav>
-                <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Inicia Sesión
+                <button className="inline-flex items-center bg-indigo-600 border-0 py-1 px-4 focus:outline-none  text-white rounded text-base mt-4 md:mt-0">Inicia Sesión
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,27 @@
-import { useContext, useEffect, useState } from "react"
+import { CSSProperties, useContext, useEffect, useState } from "react"
 import SearchIcon from '@mui/icons-material/Search';
 import { CategoriesContext } from "../context/categories.context";
 import { Product } from "../interfaces/product.interface";
 import { NavLink } from "react-router-dom";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override: CSSProperties = {
+    display: "block",
+    margin: "30px auto",
+};
 
 export const Home: React.FC = () => {
     const { productsFiltered, categorySelected } = useContext(CategoriesContext)
     const [resultsOfSearch, setResultsOfSearch] = useState<Product[]>([])
     const [searcher, setSearcher] = useState<string>("")
+    const [loading, setLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 700)
+    }, [])
 
     useEffect(() => {
         if (searcher.length === 0) {
@@ -16,12 +30,23 @@ export const Home: React.FC = () => {
     }, [searcher])
 
     useEffect(() => {
-        setResultsOfSearch(productsFiltered)
+        setLoading(true)
+        setTimeout(() => {
+            setResultsOfSearch(productsFiltered)
+            setLoading(false)
+        }, 700)
     }, [productsFiltered])
 
     const search = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        setResultsOfSearch(productsFiltered.filter(p => p.name.toLowerCase().includes(searcher.toLowerCase())))
+        if (searcher.length === 0) {
+            return
+        }
+        setLoading(true)
+        setTimeout(() => {
+            setResultsOfSearch(productsFiltered.filter(p => p.name.toLowerCase().includes(searcher.toLowerCase())))
+            setLoading(false)
+        }, 700)
     }
 
     return (
@@ -34,37 +59,50 @@ export const Home: React.FC = () => {
                         id="name"
                         name="name"
                         value={searcher}
-                        className="w-3/4 px-4 mr-1 py-2 border border-gray-300 rounded-l-lg outline-none"
+                        className="w-3/4 px-4 mr-1 py-1 bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 leading-8 transition-colors duration-200 ease-in-out"
                         onChange={(e) => setSearcher(e.target.value)}
                     />
-                    <button type="submit" className="w-1/4 px-4 py-2 rounded-r-lg bg-black text-white">
+                    <button type="submit" className="w-1/4 px-4 py-2 rounded-r-lg bg-black text-white focus:outline-none hover:bg-indigo-600">
                         <SearchIcon />
                     </button>
                 </form>
             </div>
-            <h1 className='max-w-400px my-6 mx-auto flex items-center justify-center text-gray-600 body-font'>Resultados en categoría {categorySelected}</h1>
-            <section className="text-gray-600 body-font">
-                <div className="container px-5 pb-14 mx-auto">
-                    <div className="flex flex-wrap -m-4">
-                        {
-                            resultsOfSearch.map((p, index) => {
-                                return (
-                                    <div key={index} className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                                        <NavLink to={`productDetail/${p.id}`} className="block relative h-48 rounded overflow-hidden cursor-pointer">
-                                            <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
-                                        </NavLink>
-                                        <div className="mt-4">
-                                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{p.category}</h3>
-                                            <h2 className="text-gray-900 title-font text-lg font-medium">{p.name}</h2>
-                                            <p className="mt-1">S/{p.price}</p>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            </section>
-        </div>
+            {
+                loading ? (<ClipLoader
+                    color={"#000"}
+                    loading={loading}
+                    cssOverride={override}
+                    size={50}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                />) : (
+                    <>
+                        <h1 className='max-w-400px my-6 mx-auto flex items-center justify-center text-gray-600 body-font'>Resultados en categoría {categorySelected}</h1>
+                        <section className="text-gray-600 body-font">
+                            <div className="container px-5 pb-14 mx-auto">
+                                <div className="flex flex-wrap -m-4">
+                                    {
+                                        resultsOfSearch.map((p, index) => {
+                                            return (
+                                                <div key={index} className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                                                    <NavLink to={`productDetail/${p.id}`} className="block relative h-48 rounded overflow-hidden cursor-pointer">
+                                                        <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
+                                                    </NavLink>
+                                                    <div className="mt-4">
+                                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{p.category}</h3>
+                                                        <h2 className="text-gray-900 title-font text-lg font-medium">{p.name}</h2>
+                                                        <p className="mt-1">S/{p.price}</p>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </section>
+                    </>
+                )
+            }
+        </div >
     )
 }
